bef8a9cea61cd27d314424f1da46a97413da8a34
HuynhKhuong <thebestman2204@gmail.com>
2023-01-01 19:47:26 +0700

    clean code, print out result

--- Week4/main.cpp
+++ Week4/main.cpp
@@ -8,17 +8,10 @@ stack::Stack final_stack;
 
 int main(){
   Game _mygame;
-  Game _testgame = _mygame;
 
-  std::cout <<  "check length: " << _mygame.get_Stack_length() << std::endl;
-
-  _mygame.solve(_testgame, middle_stack, final_stack);
+  _mygame.solve(_mygame, middle_stack, final_stack);
   
-  // std::cout << "check result: "<< (_testgame.get_top_stack()).get_color() << std::endl;
-  // _testgame.remove_top_Stack();
-  // std::cout << "check result: "<< (_testgame.get_top_stack()).get_color() << std::endl;
-  // _testgame.remove_top_Stack();
-  // std::cout << "check length: " <<  _testgame.get_Stack_length() << std::endl;
+  std::cout <<  "after solve length: " << _mygame.get_Stack_length() << std::endl;
 
   std::cout << "check result: "<< (final_stack.get_top_stack()).get_color() << std::endl;
   final_stack.remove_top_Stack();
