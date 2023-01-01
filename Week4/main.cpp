b215ce11ac31bd057c48550491d4e8da976c6163
HuynhKhuong <thebestman2204@gmail.com>
2023-01-01 15:01:49 +0700

    clean up main file for solve implementation

--- Week4/main.cpp
+++ Week4/main.cpp
@@ -4,16 +4,7 @@
 
 int main(){
   Game _mygame;
-  std::cout << "testing purpose" << std::endl;
-  std::cout << "length of the initial stack is: " << _mygame.get_Stack_length()<< std::endl;
-  std::cout << "color of each cube is " << _mygame.get_top_stack().get_color()<<std::endl;
-  _mygame.remove_top_Stack();
-  std::cout << "color of each cube is " << _mygame.get_top_stack().get_color()<<std::endl;
-  _mygame.remove_top_Stack();
-  std::cout << "color of each cube is " << _mygame.get_top_stack().get_color()<<std::endl;
-  _mygame.remove_top_Stack();
-  std::cout << "color of each cube is " << _mygame.get_top_stack().get_color()<<std::endl;
-  _mygame.remove_top_Stack();
+  
   _mygame.solve();
   return 0;
 }
